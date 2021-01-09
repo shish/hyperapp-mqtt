c306fa75f787dc63f36f63c9655d9b45d618c6c4
Shish <shish@shishnet.org>
2021-01-09 19:20:10 +0000

    maybe working now

--- src/index.js
+++ src/index.js
@@ -1,22 +1,20 @@
-import Paho from "paho-mqtt";
-
-export function makeRemoveListener(attachTo, dispatch, action, eventName) {
-  var handler = dispatch.bind(null, action);
-  attachTo.addEventListener(eventName, handler);
-  return function() {
-    attachTo.removeEventListener(eventName, handler);
-  };
-}
+import mqtt from 'mqtt';
 
 var mqttConnections = {};
-
+// https://test.mosquitto.org
 export function getOpenMQTT(props) {
   var connection = mqttConnections[props.url];
   if (!connection) {
     connection = {
-      socket: new Paho.MQTT.Client(props.url),
-      listeners: []
+      socket: mqtt.connect(props.url), // new Paho.MQTT.Client(props.url),
+      listeners: {}
     };
+    connection.socket.on('connect', function() {
+      Object.keys(connection.listeners).map(t => connection.socket.subscribe(t));
+    })
+    connection.socket.on('message', function(topic, payload, packet) {
+      connection.listeners[topic](payload.toString());
+    })
     mqttConnections[props.url] = connection;
   }
   return connection;
@@ -25,51 +23,20 @@ export function getOpenMQTT(props) {
 export function closeMQTT(props) {
   var connection = getOpenMQTT(props);
   // FIXME: handle close on opening
-  connection.socket.disconnect();
+  connection.socket.end();
   delete mqttConnections[props.url];
 }
 
 function mqttListenEffect(dispatch, props) {
   var connection = getOpenMQTT(props);
-  var removeMessageArrived = makeRemoveListener(
-    connection.socket,
-    dispatch,
-    props.messageArrived,
-    "messageArrived"
-  );
-  connection.listeners.push(removeMessageArrived);
-  var removeConnected;
-  if (props.open) {
-    removeConnected = makeRemoveListener(
-      connection.socket,
-      dispatch,
-      props.open,
-      "connected"
-    );
-    connection.listeners.push(removeConnected);
-  }
-  var removeConnectionLost;
-  if (props.close) {
-    removeConnectionLost = makeRemoveListener(
-      connection.socket,
-      dispatch,
-      props.close,
-      "close"
-    );
-    connection.listeners.push(removeConnectionLost);
+
+  connection.listeners[props.topic] = dispatch.bind(null, props.message)
+  if(connection.socket.connected) {
+    connection.socket.subscribe(props.topic);
   }
 
   return function() {
-    removeMessageArrived && removeMessageArrived();
-    removeConnected && removeConnected();
-    removeConnectionLost && removeConnectionLost();
-    connection.listeners = connection.listeners.filter(function(listener) {
-      return (
-        listener !== removeMessageArrived &&
-        listener !== removeConnected &&
-        listener !== removeConnectionLost
-      );
-    });
+    delete connection.listeners[props.topic];
     if (connection.listeners.length === 0) {
       closeMQTT(props);
     }
--- test/index.test.js
+++ test/index.test.js
@@ -1,7 +1,24 @@
-import { MQTTListen } from "../src";
+import { getOpenMQTT, closeMQTT, MQTTListen } from "../src";
 
 describe("MQTTListen", () => {
   it("should initialise", () => {
-    MQTTListen({ url: "ws://example.com/mqtt" });
+    MQTTListen({ url: "ws://test.mosquitto.org/mqtt" });
+  });
+});
+
+describe("Connections", () => {
+  it("should open", () => {
+    getOpenMQTT({ url: "ws://test.mosquitto.org/mqtt" });
+  });
+  it("shouldn't duplicate", () => {
+    let a = getOpenMQTT({ url: "ws://test.mosquitto.org/mqtt" });
+    let b = getOpenMQTT({ url: "ws://test.mosquitto.org/mqtt" });
+    expect(a).toEqual(b);
+  });
+  it("should close", () => {
+    let a = getOpenMQTT({ url: "ws://test.mosquitto.org/mqtt" });
+    closeMQTT({ url: "ws://test.mosquitto.org/mqtt" });
+    let b = getOpenMQTT({ url: "ws://test.mosquitto.org/mqtt" });
+    expect(a).not.toEqual(b);
   });
 });

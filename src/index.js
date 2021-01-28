17497e0efaa5f9f17b50fbe8b1a1f00425d9bc2f
Shish <shish@shishnet.org>
2021-01-28 01:29:01 +0000

    more tests

--- src/index.js
+++ src/index.js
@@ -8,17 +8,18 @@ export function getOpenMQTT(props) {
     connection = {
       socket: mqtt.connect(props.url, props.options),
       connect_listeners: [],
-      topic_listeners: {},
+      message_listeners: [],
       close_listeners: []
     };
     connection.socket.on("connect", function() {
-      Object.keys(connection.topic_listeners).map(t =>
+      Object.keys(connection.message_listeners).map(t =>
         connection.socket.subscribe(t)
       );
       connection.connect_listeners.map(l => l());
     });
     connection.socket.on("message", function(topic, _payload, packet) {
-      connection.topic_listeners[topic](packet);
+      // TODO support + and # in subscriptions
+      connection.message_listeners.map(([t, l]) => t === topic && l(packet));
     });
     connection.socket.on("close", function() {
       connection.close_listeners.map(l => l());
@@ -38,22 +39,42 @@ export function closeMQTT(props) {
 function mqttSubscribeEffect(dispatch, props) {
   var connection = getOpenMQTT(props);
 
-  connection.topic_listeners[props.topic] = dispatch.bind(null, props.message);
+  // if we created a new connection, then it included a call to subscribe
+  // if we were already connected, we need to do it ourselves
   if (connection.socket.connected) {
     connection.socket.subscribe(props.topic);
   }
 
-  if (props.connect) {
-    connection.connect_listeners.push(dispatch.bind(null, props.connect));
+  let my_onmessage = null;
+  if (props.message) {
+    my_onmessage = [props.topic, dispatch.bind(null, props.message)];
+    connection.message_listeners.push(my_onmessage);
   }
+
+  let my_onconnect = null;
+  if (props.connect) {
+    my_onconnect = dispatch.bind(null, props.connect);
+    connection.connect_listeners.push(my_onconnect);
+  }
+
+  let my_onclose = null;
   if (props.close) {
-    connection.close_listeners.push(dispatch.bind(null, props.close));
+    my_onclose = dispatch.bind(null, props.close)
+    connection.close_listeners.push(my_onclose);
   }
 
   return function() {
-    // TODO: remove our connect & close listeners when we unsubscribe
-    delete connection.topic_listeners[props.topic];
-    if (connection.topic_listeners.length === 0) {
+    // Remove the listeners which we added
+    connection.message_listeners = connection.message_listeners.filter(x => x != my_onmessage);
+    connection.connect_listeners = connection.connect_listeners.filter(x => x != my_onconnect);
+    connection.close_listeners = connection.close_listeners.filter(x => x != my_onclose);
+
+    // if no more listeners, close the socket
+    if (
+      connection.messagt_listeners.length === 0 &&
+      connection.connect_listeners.length === 0 &&
+      connection.close_listeners.length === 0
+    ) {
       closeMQTT(props);
     }
   };
--- test/index.test.js
+++ test/index.test.js
@@ -1,53 +1,118 @@
 import { getOpenMQTT, closeMQTT, MQTTSubscribe, MQTTPublish } from "../src";
+import mqtt from "mqtt";
 
-let props = {
-  url: "ws://test.mosquitto.org/mqtt",
-  topic: "test/hyperapp-mqtt",
-  payload: "hello world",
-};
-let authprops = {
-  url: "ws://test.mosquitto.org/mqtt",
-  options: {username: 'test', password: 'test'},
-  topic: "test/hyperapp-mqtt"
-}
+let url = "mqtt://test.mosquitto.org/";
+let topic = "test/hyperapp-mqtt";
+let payload = "hello world";
+let options = {username: 'test', password: 'test'};
 
+// just testing that our foundations work as expected
+describe("MQTT", () => {
+  it("should open & close", done => {
+    var client = mqtt.connect(url)
+    client.on('connect', function () {
+      client.subscribe(topic, function (err) {
+        if (!err) {
+          client.publish(topic, 'hello from mqtt')
+        }
+      })
+    })
+    client.on('message', function (topic, message) {
+      client.end()
+      done()
+    })
+  });
+});
+
+// open & close
+describe("Connections", () => {
+  it("should open & close", done => {
+    let props = {url, topic};
+    let c = getOpenMQTT(props);
+    c.socket.on("connect", function() {
+      closeMQTT(props);
+    });
+    c.socket.on("close", function() {
+      done();
+    });
+  });
+  it("should cache for the same DSN", done => {
+    let props = {url, topic};
+    let a = getOpenMQTT(props);
+    a.socket.on("connect", function() {closeMQTT(props);});
+    a.socket.on("close", function() {done();});
+    let b = getOpenMQTT(props);
+    expect(a).toEqual(b);
+  });
+  it("should not cache for different DSN", done => {
+    let props = {url, topic};
+    let authprops = {url, topic, options};
+    let a = getOpenMQTT(props);
+    a.socket.on("connect", function() {
+      let b = getOpenMQTT(authprops);
+      expect(a).not.toEqual(b);
+      b.socket.on("connect", function() {closeMQTT(authprops);});
+      b.socket.on("close", function() {closeMQTT(props);});
+    });
+    a.socket.on("close", function() {done();});
+  });
+  it("should not cache after close", done => {
+    let props = {url, topic};
+    let a = getOpenMQTT(props);
+    let b = null;
+    a.socket.on("connect", function() {closeMQTT(props);});
+    a.socket.on("close", function() {
+      b = getOpenMQTT(props);
+      expect(a).not.toEqual(b);
+      b.socket.on("connect", function() {closeMQTT(props);});
+      b.socket.on("close", function() {done();});  
+    });
+  });
+});
+
+/*
 describe("MQTTSubscribe", () => {
-  it("should initialise", () => {
-    let dispatch = {bind: x => x};
+  it("should subscribe and unsubscribe", done => {
+    let dispatch = {bind: (n, x) => x};
+    let props = {url, topic, close: () => {done();}};
+    let sub = MQTTSubscribe(props);
+    let unsub = sub[0](dispatch, sub[1]);
+    unsub();
+  });
+  it("should receive messages", done => {
+    let dispatch = {bind: (n, x) => x};
+    let props = {url, topic, close: () => {done();}};
     let sub = MQTTSubscribe(props);
     let unsub = sub[0](dispatch, sub[1]);
     unsub();
   });
 });
+*/
 
 describe("MQTTPublish", () => {
-  it("should initialise", () => {
-    let dispatch = {bind: x => x};
-    let fx = MQTTPublish(props);
-    fx[0](dispatch, fx[1]);
-    closeMQTT(props);
-  });
-});
+  it("should publish", done => {
 
-describe("Connections", () => {
-  it("should open & close", () => {
-    getOpenMQTT(props);
-    closeMQTT(props);
-  });
-  it("should cache for the same DSN", () => {
-    let a = getOpenMQTT(props);
-    let b = getOpenMQTT(props);
-    expect(a).toEqual(b);
-  });
-  it("should not cache for different DSN", () => {
-    let a = getOpenMQTT(props);
-    let b = getOpenMQTT(authprops);
-    expect(a).not.toEqual(b);
-  });
-  it("should close", () => {
-    let a = getOpenMQTT(props);
-    closeMQTT(props);
-    let b = getOpenMQTT(props);
-    expect(a).not.toEqual(b);
+    let c = getOpenMQTT({url});
+    let client = c.socket;
+    client.on('connect', function () {
+      client.subscribe(topic, function (err) {
+
+        let dispatch = {bind: (n, x) => x};
+        let fx = MQTTPublish({
+          url,
+          topic,
+          payload: "Hello from MQTTPublish",
+        });
+        fx[0](dispatch, fx[1]);
+    
+      })
+    })
+    client.on('message', function (topic, message) {
+      expect(message.toString()).toEqual("Hello from MQTTPublish");
+      closeMQTT({url})
+    })
+    client.on('close', function () {
+      done()
+    })
   });
 });
